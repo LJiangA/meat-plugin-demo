f1ae66c7e0ba27a370332b4cb68f7ac75c751551
GitHub Action <action@github.com>
2025-11-18 09:45:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/ljianga/meatplugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ljianga/meatplugindemo/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ljianga.meatplugindemo.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ljianga.meatplugindemo.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
